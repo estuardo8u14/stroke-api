a37a013a8ea342d7d6d22ae76047b5c0f6d35c84
estuardo8u14 <estuardo8u14@gmail.com>
2022-10-25 18:05:20 -0600

    cambio

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["Content-Type"] + get_cors_allowed_headers(),
+    allow_headers=["*"] + get_cors_allowed_headers(),
     expose_headers=["*"]
 )
 
